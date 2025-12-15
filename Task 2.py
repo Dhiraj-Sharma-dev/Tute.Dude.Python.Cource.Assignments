b8963d3ba32c73b44c60c5d4560944db5ee5172d
Dhiraj-Sharma-dev <naruto8278278@gmail.com>
2025-12-15 21:33:48 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,14 @@
+# Task 2
+
+# Step 1 is to take a user's first name and last name as input.
+
+user_first_name = input("Enter your first name: ")
+user_last_name = input("Enter your last name: ")
+
+# Step 2 is to concatenates the first name and last name into a full name.
+
+user_name = user_first_name + " " +user_last_name
+
+# Step 3 is to print a personalized greeting message using the full name.
+
+print("Hello,", user_name, "! Welcome to the Python programme")
---
+++ Task1.py
@@ -0,0 +1,20 @@
+# Task 1
+
+# Step one is to take two numbers as input from the user:
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+# Basic mathematical operations from those two numbers:
+#Addition
+Addition = number_1 + number_2
+print("Addition:", Addition)
+#Substraction
+Subtraction = number_1 - number_2
+print("Subtraction:", Subtraction)
+#Multiplication
+Multiplication = number_1 * number_2
+print("Multiplication:", Multiplication)
+#Division
+Division = number_1 / number_2
+print("Division:", Division)
